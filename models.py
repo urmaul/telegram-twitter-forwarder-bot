b99112fbf3c7d3ff470df96c8bf95233ec0bb1b4
urmaul <me@urmaul.com>
2018-03-14 22:05:21 +0100

    Media groups support + a lot of rewriting

--- models.py
+++ models.py
@@ -37,43 +37,37 @@ class TelegramChat(Model):
         return tweepy.API(auth)
 
 
-class Tweet(Model):
-    tw_id = BigIntegerField(unique=True)
-    known_at = DateTimeField(default=datetime.datetime.now)
-    text = TextField()
-    created_at = DateTimeField()
-    twitter_user_name = TextField(default='')
-    twitter_user_screen_name = TextField(default='')
-    photo_url = TextField(default='')
-    video_url = TextField(default='')
-    link_url = TextField(default='')
+class Media:
+    def __init__(self, type, url):
+        self.type = type
+        self.url = url
 
-    @property
-    def screen_name(self):
-        return self.twitter_user_screen_name
+class Tweet:
+    def __init__(self, *args, **kwargs):
+        self.id = None
+        self.text = None
+        self.created_at = None
+        self.user_name = None
+        self.user_screen_name = None
+        self.link_url = ''
+        self.media_list = []
 
-    @property
-    def name(self):
-        return self.twitter_user_name
-
+        for k in kwargs:
+            setattr(self, k, kwargs[k])
 
 # Create tables
-for t in (TelegramChat, Tweet):
-    t.create_table(fail_silently=True)
-
+TelegramChat.create_table(fail_silently=True)
 
 # Migrate new fields. TODO: think of some better migration mechanism
 db = SqliteDatabase('peewee.db', timeout=10)
 migrator = SqliteMigrator(db)
 operations = [
-    migrator.add_column('tweet', 'photo_url', Tweet.photo_url),
     migrator.add_column('telegramchat', 'twitter_request_token', TelegramChat.twitter_request_token),
     migrator.add_column('telegramchat', 'twitter_token', TelegramChat.twitter_token),
     migrator.add_column('telegramchat', 'twitter_secret', TelegramChat.twitter_secret),
     migrator.add_column('telegramchat', 'timezone_name', TelegramChat.timezone_name),
     migrator.add_column('telegramchat', 'delete_soon', TelegramChat.delete_soon),
     migrator.add_column('telegramchat', 'last_tweet_id', TelegramChat.last_tweet_id),
-    migrator.add_column('tweet', 'twitter_user_name', Tweet.twitter_user_name),
 ]
 for op in operations:
     try:
